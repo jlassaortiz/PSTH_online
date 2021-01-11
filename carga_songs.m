ff53b11bd9afff531f9deb8f9e44c913629ffebc
jlassaortiz <javi.lassa@gmail.com>
2021-01-11 17:58:40 -0300

    carga songs

--- carga_songs.m
+++ carga_songs.m
@@ -1,13 +1,22 @@
 
 % Crea el archivo 'songs.mat' con los .wav que encuentra en el directorio
-files=dir(fullfile('*.wav'));
+files = dir(fullfile('*.wav'));
+n = length(files);
 % Esto carga los archivos BOS,CON y REV (orden alfabetico)
 % Carga BOS
-BOS=audioread(files(1).name);
+BOS = audioread(files(1).name);
 % Carga CON
-CON=audioread(files(2).name);
-% Carga REV
-[REV, sound_fs]=audioread(files(3).name);
+CON = audioread(files(2).name);
+% Carga REV y frecuencia de sampleo de los estímulos
+[REV, sound_fs] = audioread(files(3).name);
 
-clear files
+% Cargo todos los SYN y el nombre de los archivos en un struct
+SYN = struct;
+
+for i = 4:n
+   SYN(i-3).name = files(i).name;
+   SYN(i-3).song = audioread(files(i).name);
+end
+
+clear files n
 %save songs.mat
