3f426067ee6ef93d4480bd276bccee9f8e956b9e
jlassaortiz <javi.lassa@gmail.com>
2021-01-13 12:44:59 -0300

    mejoro carga_songs
    carga_songs ahora lee todos los .wav que hay en el directorio que se le indique y genera un struct con el nombre completo (direccion y nombre del audio) junto al audio

--- carga_songs.m
+++ carga_songs.m
@@ -1,22 +1,14 @@
 
 % Crea el archivo 'songs.mat' con los .wav que encuentra en el directorio
-files = dir(fullfile('*.wav'));
+files = dir(horzcat(directorio,'*.wav'));
 n = length(files);
-% Esto carga los archivos BOS,CON y REV (orden alfabetico)
-% Carga BOS
-BOS = audioread(files(1).name);
-% Carga CON
-CON = audioread(files(2).name);
-% Carga REV y frecuencia de sampleo de los estímulos
-[REV, sound_fs] = audioread(files(3).name);
 
-% Cargo todos los SYN y el nombre de los archivos en un struct
-SYN = struct;
+% Cargo todos los estimulos y el nombre de los archivos en un struct
+estimulo = struct;
 
-for i = 4:n
-   SYN(i-3).name = files(i).name;
-   SYN(i-3).song = audioread(files(i).name);
+for i = 1:n
+   estimulo(i).name = horzcat(directorio, files(i).name);
+   estimulo(i).song = audioread(estimulo(i).name);
 end
 
-clear files n
-%save songs.mat
+clear files n i
