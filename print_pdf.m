35ef6326ff2e8833821faa21383644928af3bcc7
jlassarti <javi.lassa@gmail.com>
2021-02-10 18:06:31 -0300

    add extra-name print_pdf
    agrego opción de agregar nombre extra a print pdf. Ahora genera un gráfico con la fecha y hora y al final el nombre extra

--- print_pdf.m
+++ print_pdf.m
@@ -1,9 +1,9 @@
-function print_pdf(fig_number, nombre, directorio)
+function print_pdf(fig_number, directorio, nombre_extra)
 
 % Guarda la figura especificada en formato PDF
 
 f = figure(fig_number);
-nombre = strcat(directorio, datestr(now, 'yyyy-mm-dd_HH_MM_SS')," ", nombre);
+nombre = strcat(directorio, datestr(now, 'yyyy-mm-dd_HH_MM_SS'), nombre_extra);
 
 set(f,...
     'Units','centimeters', ...
