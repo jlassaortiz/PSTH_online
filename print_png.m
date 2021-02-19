8923490c30476689f735d92f5dca9a661885416e
jlassarti <javi.lassa@gmail.com>
2021-02-19 15:35:51 -0300

    bug. ahora anda matlab 2019b

--- print_png.m
+++ print_png.m
@@ -6,13 +6,7 @@ f = figure(fig_number);
 nombre = strcat(directorio, datestr(now, 'yyyy-mm-dd_HH_MM_SS'), nombre_extra);
 nombre = strcat(nombre, '.png');
 
-% set(f,...
-%     'Units','centimeters', ...
-%     'PaperUnits', 'centimeters' , ...
-%     'PaperSize',[60 46])
-
-% print(nombre,f,'-dpng') % ,'-r300'  hace referencia a los dpi
-exportgraphics(f,nombre, 'Resolution', 100)
+print(f, nombre,'-dpng','-r100')
 
 end
 
