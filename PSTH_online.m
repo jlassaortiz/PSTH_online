f9c16aa9eeba96c2ef9611cc7b5f4aa5c56543be
jlassarti <javi.lassa@gmail.com>
2021-02-25 15:26:39 -0300

    cambio estetico

--- PSTH_online.m
+++ PSTH_online.m
@@ -1,9 +1,20 @@
 % Script que hace todo de una
-close all
-clear all
 
+% Defino directorio
 directorio = input('Directorio: ','s');
 directorio = horzcat(directorio , '/');
+
+% Eligo un canal de un puerto en especifico
+puerto = input('Puerto a levantar: ','s');
+canal = input('Canal de INTAN a filtrar (X-0XX):  ');
+puerto_canal = [puerto '-0' num2str(canal,'%.2d')];
+
+% Cargamos cantidad de trials y tiempo que dura cada uno
+ntrials = input('Numero de trials: ');
+tiempo_file = input('Tiempo entre estimulos (en s): ');
+
+% Definimos un umbral para threshold cutting (en uV)
+thr = input('Threshold para el threshold cutting (en uV):  ');
 
 % Genero songs.mat a partir de las canciones
 estimulos = carga_songs(directorio);
@@ -11,11 +22,6 @@ estimulos = carga_songs(directorio);
 % Leer info INTAN
 read_Intan_RHD2000_file(horzcat(directorio, 'info.rhd'));
 clear notes spike_triggers supply_voltage_channels aux_input_channels 
-
-% Eligo un canal de un puerto en especifico
-puerto = input('Puerto a levantar: ','s');
-canal = input('Canal de INTAN a filtrar (X-0XX):  ');
-puerto_canal = [puerto '-0' num2str(canal,'%.2d')];
 
 % Levanto el canal de interes
 raw = read_INTAN_channel(directorio, puerto_canal, amplifier_channels);
@@ -28,18 +34,11 @@ filt_spikes = designfilt('highpassiir','DesignMethod','butter','FilterOrder',...
 raw_filtered = filtfilt(filt_spikes, raw);
 clear puerto canal filt_spikes
 
-% Definimos un umbral para threshold cutting (en uV)
-thr = input('Threshold para el threshold cutting (en uV):  ');
-
 % Buscamos spike por threshold cutting
 spike_times = find_spike_times(raw_filtered, thr, frequency_parameters);
 
 % Carga datos filtrados y hace un threshold cutting
 plot_spikes_shapes(raw_filtered, spike_times, thr, frequency_parameters, directorio)
-
-% Cargamos cantidad de trials y tiempo que dura cada uno
-ntrials = input('Numero de trials: ');
-tiempo_file = input('Tiempo entre estimulos (en s): ');
 
 % Genero diccionario con nombre de los estimulos y el momento de presentacion
 t0s_dictionary = find_t0s(estimulos, ntrials, tiempo_file, board_adc_channels, frequency_parameters, directorio);
