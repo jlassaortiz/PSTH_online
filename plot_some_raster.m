357015034f757db01987db74832a295bedd6a61c
jlassarti <javi.lassa@gmail.com>
2021-03-04 16:39:03 -0300

    plot_some_raster mejora!
    ahora la función plot_some_raster plotea en grilla con tres columnas, ademas pone de titulo a cada PSTH el resultado de la integral de spikes normalizada (cantidad de spikes durante el canto menos cantidad de spikes en el silencio posterior de igual duracion)

--- plot_some_raster.m
+++ plot_some_raster.m
@@ -3,45 +3,71 @@ function plot_some_raster(id_estimulos, estimulos, rasters, frequency_parameters
 % plot_raster plotea el raster y psth del número de estímulo indicado
 %   Detailed explanation goes here
 
-psth_max = ntrials ;
+psth_max = ntrials;
 
 figure()
-n = 5 * round(length(id_estimulos)/2);
-m = 2;    
+
+if mod(length(id_estimulos), 3) == 0
+    n = 5 * length(id_estimulos)/3;
+    
+elseif mod(length(id_estimulos), 3) == 2
+    n = 5 * round(length(id_estimulos)/3);
+    
+else
+    n = 5 * (round(length(id_estimulos)/3) + 1);
+end
+    
+m = 3;    
 j = 0;
 k = 0;
 
-for i = id_estimulos
+for i = id_estimulos % para cada est�mulo
     
     k = k + 1;
     
-    if mod(k, 2) == 1
-        p = (k - 1)/2 * 10 + 1;
+    if mod(k, 3) == 1
+        p = (k - 1)/3 * 15 + 1;
+        
+    elseif mod(k, 3) == 2
+        p = (k-2)/3 * 15 + 2;
+        
     else
-        p = ((k / 2) - 1) * 10 + 2;
+        p = ((k / 3) - 1) * 15 + 3;
     end
     
     % sonido
     j = j + 1;
-    h(1) = subplot(n, m , p);
+    h(j) = subplot(n, m , p);
     plot(1000/estimulos(i).freq * (0:1:(length(estimulos(i).song) -1)), estimulos(i).song,'black')
     hold on;
     line([0 tiempo_file*1000],[0 0],'color',[0 0 0]);
     xlim([0 tiempo_file * 1000])
-    title(strcat(string(i), " - ",estimulos(i).name), 'Interpreter','None')
+    title(strcat(string(i), " - ",estimulos(i).name), 'Interpreter','None', 'FontSize',6)
 
     % psth
     j = j + 1;
-    h(2) = subplot(n, m, [p + 2, p + 4]);
+    h(j) = subplot(n, m, [p + 3, p + 6]);
     histogram(rasters(i).spikes_norm * 1000/frequency_parameters.amplifier_sample_rate , ...
         (1000/frequency_parameters.amplifier_sample_rate) * (-1000:(0.015*frequency_parameters.amplifier_sample_rate):(tiempo_file*frequency_parameters.amplifier_sample_rate)) );
     ylim([0 psth_max]);
     xlim([0 tiempo_file * 1000]);
     
+    % Guardo la frecuencia de sampleo y el largo (en seg) de este estimulo
+    song_freq = estimulos(i).freq;
+    song_len = length(estimulos(i).song) / song_freq; % unidades: seg
+    
+    % Integracion de spikes normalizada
+    integral = sum(rasters(i).spikes_norm < song_len * frequency_parameters.amplifier_sample_rate);
+    ruido    = sum(rasters(i).spikes_norm > song_len * frequency_parameters.amplifier_sample_rate & ...
+        rasters(i).spikes_norm < song_len * 2 * frequency_parameters.amplifier_sample_rate); 
+    integral_norm = integral - ruido;
+    integral_text = strcat('integral_norm: ', string(integral_norm));
+    
+    title(integral_text , 'Interpreter','None')
 
     % raster
     j = j + 1;
-    h(j) = subplot(n, m, [p + 6, p + 8]);
+    h(j) = subplot(n, m, [p + 9, p + 12]);
     plot((1000/frequency_parameters.amplifier_sample_rate) * rasters(i).spikes_norm, rasters(i).trials_id, '.')  
     xlim([0 tiempo_file * 1000])
     ylim([0 ntrials + 1])
