64475aa7ad72b0435da498c0cbdcd48f4708c24a
jlassarti <javi.lassa@gmail.com>
2021-03-09 16:08:11 -0300

    sumo sliding window al analisis
    Ahora la funcion plot_some_raster.m calcula la sliding window de todos los psth (usando la funcion nueva llamada sliding_window.m). Ademas, calcula el coeficiente de correlacion de pearson de todas las sw con la sw del BOS (NORMALIZADAS, TODAS). Finalmente, el ylim del psth estaba harcodeado, logre hacerlo automatico, pero es necesario especificar el numero de id que el BOS tiene en el objeto estimulos

--- plot_some_raster.m
+++ plot_some_raster.m
@@ -1,12 +1,21 @@
-function plot_some_raster(id_estimulos, estimulos, rasters, frequency_parameters, tiempo_file, ntrials, puerto_canal, thr, directorio)
-% 
-% plot_raster plotea el raster y psth del n√∫mero de est√≠mulo indicado
+function plot_some_raster(id_estimulos, id_BOS, estimulos, rasters, frequency_parameters, tiempo_file, ntrials, puerto_canal, thr, directorio)
+
+% plot_raster plotea el raster y psth del numero de estimulos indicados
 %   Detailed explanation goes here
 
-psth_max = ntrials;
+t_window = 0.015; % 15 ms
+step = 0.001; % 1 ms
 
+% Busco el maximo de los psth
+hist_aux = histcounts(rasters(id_BOS).spikes_norm * 1000/frequency_parameters.amplifier_sample_rate , ...
+        (1000/frequency_parameters.amplifier_sample_rate) * (-1000:(0.015* ... 
+        frequency_parameters.amplifier_sample_rate):(tiempo_file*frequency_parameters.amplifier_sample_rate)) );
+psth_max = max(hist_aux) * 1.2; % ylim de los psth es un 20% mas que el maximo del BOS
+
+% Inicializo figura
 figure()
 
+% Formula para armar grilla segun la cantidad de estimulos a analizar
 if mod(length(id_estimulos), 3) == 0
     n = 5 * length(id_estimulos)/3;
     
@@ -20,6 +29,16 @@ end
 m = 3;    
 j = 0;
 k = 0;
+
+% Calculo la sw del BOS para poder hacer correlaciones con el resto
+[sw_data_BOS, sw_times_BOS] = sliding_window(rasters(id_BOS).spikes_norm, frequency_parameters.amplifier_sample_rate, ...
+        t_window, step);
+    
+% Conservo solo la seccion donde se presenta el estimulo auditivo
+duracion_BOS = length(estimulos(id_BOS).song) / estimulos(id_BOS).freq; 
+sw_data_BOS = sw_data_BOS(sw_times_BOS < duracion_BOS);
+sw_times_BOS = sw_times_BOS(sw_times_BOS < duracion_BOS);
+sw_data_BOS_norm = sw_data_BOS / max(sw_data_BOS);
 
 for i = id_estimulos % para cada estÌmulo
     
@@ -42,15 +61,25 @@ for i = id_estimulos % para cada estÌmulo
     hold on;
     line([0 tiempo_file*1000],[0 0],'color',[0 0 0]);
     xlim([0 tiempo_file * 1000])
+    xticks([])
+    
     title(strcat(string(i), " - ",estimulos(i).name), 'Interpreter','None', 'FontSize',6)
 
     % psth
     j = j + 1;
     h(j) = subplot(n, m, [p + 3, p + 6]);
-    histogram(rasters(i).spikes_norm * 1000/frequency_parameters.amplifier_sample_rate , ...
-        (1000/frequency_parameters.amplifier_sample_rate) * (-1000:(0.015*frequency_parameters.amplifier_sample_rate):(tiempo_file*frequency_parameters.amplifier_sample_rate)) );
+    hist = histogram(rasters(i).spikes_norm * 1000/frequency_parameters.amplifier_sample_rate , ...
+        (1000/frequency_parameters.amplifier_sample_rate) * (-1000:(0.015* ... 
+        frequency_parameters.amplifier_sample_rate):(tiempo_file*frequency_parameters.amplifier_sample_rate)) );
     ylim([0 psth_max]);
     xlim([0 tiempo_file * 1000]);
+    hold on;
+   
+    % Busco maximos en cada psth y los grafico
+    [maximo, id_max] = max(hist.Values);
+    pos_max = hist.BinEdges(id_max);
+    pos_max = pos_max + hist.BinWidth /2;
+    plot(pos_max, maximo, 'or');
     
     % Guardo la frecuencia de sampleo y el largo (en seg) de este estimulo
     song_freq = estimulos(i).freq;
@@ -63,7 +92,22 @@ for i = id_estimulos % para cada estÌmulo
     integral_norm = integral - ruido;
     integral_text = strcat('integral_norm: ', string(integral_norm));
     
-    title(integral_text , 'Interpreter','None')
+    % Calculo sliding window para cada estimulo
+    [sw_data, sw_times] = sliding_window(rasters(i).spikes_norm, frequency_parameters.amplifier_sample_rate, ...
+        t_window, step);
+    plot(sw_times * 1000, sw_data, '-b');
+    plot(sw_times_BOS * 1000 , sw_data_BOS, '-r');
+    
+    % Calculo correlaciÛn de sw normalizada con la sw normalizada del BOS
+    sw_data_norm = sw_data / max(sw_data);
+    sw_data_norm = sw_data_norm(sw_times < duracion_BOS);
+    R2 = corrcoef(sw_data_norm, sw_data_BOS_norm);
+    R2_text = strcat('R2 sw BOS norm: ' , string(round(R2(1,2), 2)));
+
+    
+    % Escribo en el titulo los valores de integral y correlacion de sw
+    % normalizadas
+    title(strcat(integral_text, ' / ' , R2_text) , 'Interpreter','None')
 
     % raster
     j = j + 1;
@@ -71,6 +115,7 @@ for i = id_estimulos % para cada estÌmulo
     plot((1000/frequency_parameters.amplifier_sample_rate) * rasters(i).spikes_norm, rasters(i).trials_id, '.')  
     xlim([0 tiempo_file * 1000])
     ylim([0 ntrials + 1])
+    xticks([])
 end 
 
 % Linkeo eje x (no se pueden hacer varios links independientes juntos)
@@ -79,7 +124,8 @@ linkaxes(h, 'x');
 % Titulo general
 sgtitle({datestr(now, 'yyyy-mm-dd'); ...
     string(directorio) ; ...
-    strcat(string(puerto_canal), "  " , string(thr), "uV", "  ntrials:", string(ntrials), "  t_inter_estimulo:", string(tiempo_file)) }, 'Interpreter','None')
+    strcat(string(puerto_canal), "  " , string(thr), "uV", "  ntrials:", string(ntrials),  ...
+    "  t_inter_estimulo:", string(tiempo_file)) }, 'Interpreter','None')
 
 end
 
---
+++ sliding_window.m
@@ -0,0 +1,43 @@
+function [sliding_window_data, sliding_window_tiempo] = sliding_window(data, fs, t_window, step)
+
+% Toma un vector unidimensional y le calcula la slinding window (SW)
+% con los parametros de tamaño de ventana (en seg.) y tamaño del paso  
+% especificados(en seg.). Además necesita que se especifique la frecuencia 
+% de sampleo del vector unidimensional.
+%
+% data = vector unidimensional al que se le va a calcular la SW
+% fs = frecuencia de sampleo de 'data'
+% t_window = tamaño de la ventana de la SW (en seg.)
+% step = tamaño del paso de corrimiento de la SW (en seg.)
+
+
+% Calculo el tamaño final de: sliding_window_data y sliding_window_tiempo
+tf = t_window;
+n = 0;
+limite = data(end)/fs;
+
+while tf <= limite
+
+   n = n + 1;
+   tf = tf + step;
+end
+
+% Inicializo vector donde se guardan resultados y tiempos
+sliding_window_data = zeros(n, 1);
+sliding_window_tiempo = zeros(n,1);
+tf = t_window;
+ti = 0;
+
+% Calculo el valor de cada punto de la sw y el tiempo que le corresponde
+for i = (1:1:n)
+
+    punto = data( data( data > ti*fs ) <= tf*fs );
+    sliding_window_data(i) = numel(punto);
+
+    sliding_window_tiempo(i) = (ti + tf)/2;
+
+    ti = ti + step;
+    tf = tf + step;
+end
+    
+end
