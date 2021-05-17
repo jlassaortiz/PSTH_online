31dfbd6956f2eea92f605cc1b27322d746faf4a2
javi <javi.lassa@gmail.com>
2021-05-17 11:27:51 -0300

    add commentsplot_sabanas.m

--- plot_sabana.m
+++ plot_sabana.m
@@ -2,13 +2,18 @@ function plot_sabana(mat_scores, directorio, ejeY_col, ejeX_fila)
 
 % Plotea Sabana x2 (integral y correlacio) y perfiles de sabana x2
 %
-%   Detailed explanation goes here
+%   En total hace 4 ploteos:
+%     - sabana usando integral como variable respuesta
+%     - sabana usando r-pearson como variable respuesta
+%     - perfiles de las sabanas (x2)
+   
 
 X  = mat_scores(:,1);
 Y  = mat_scores(:,2);
 Z1 = mat_scores(:,3);
 Z2 = mat_scores(:,4);
 
+% Plot sabana INTEGRAL
 [xq,yq] = meshgrid(1:0.1:3);
 z = griddata(X,Y,Z1,xq,yq,'natural');
 figure()
@@ -19,6 +24,7 @@ ylabel(ejeY_col)
 xlabel(ejeX_fila)
 title({'integral', directorio}, 'Interpreter','None')
 
+% Plot sabana CORRELACION
 [xq,yq] = meshgrid(1:0.1:3);
 z = griddata(X,Y,Z2,xq,yq,'natural');
 figure()
@@ -30,6 +36,7 @@ xlabel(ejeX_fila)
 title({'correlacion', directorio}, 'Interpreter','None')
 
 
+
 % Hago diagonales para plotear perfiles
 diag_escala = mat_scores(:,1) == 1 & mat_scores(:,2) == 1 | ... 
     mat_scores(:,1) == 2 & mat_scores(:,2) == 2 | ...
@@ -39,7 +46,7 @@ diag_mostruo = mat_scores(:,1) == 1 & mat_scores(:,2) == 3 | ...
     mat_scores(:,1) == 2 & mat_scores(:,2) == 2 | ...
     mat_scores(:,1) == 3 & mat_scores(:,2) == 1 ;
 
-% Ploteo perfiles
+% Ploteo perfiles INTEGRAL
 figure()
 plot([0, 1, 2], mat_scores(diag_escala, 3))
 hold on
@@ -48,7 +55,7 @@ legend('escala', 'mostruo')
 ylabel('integral')
 title({'integral', directorio}, 'Interpreter','None')
 
-
+% Ploteo perfiles CORRELACION
 figure()
 plot([0, 1, 2], mat_scores(diag_escala, 4))
 hold on
@@ -57,6 +64,4 @@ legend('escala', 'mostruo')
 ylabel('correlacion')
 title({'correlacion', directorio}, 'Interpreter','None')
 
-
 end
-
