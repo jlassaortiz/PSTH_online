9455428b7bffb264098eb75bedc674a0b806710e
Jav <javi.lassa@gmail.com>
2022-05-27 16:13:23 -0300

    Update plot_some_raster_LFP_1tetrode.m
    grafica todo el tiempo que dura la prasentacion del estimulo

--- plot_some_raster_LFP_1tetrode.m
+++ plot_some_raster_LFP_1tetrode.m
@@ -57,8 +57,9 @@ psth_max = max(hist_aux) * 1.2;
 figure()
 
 % Para no ver tanta actividad espontanea grafico 75% mas de lo que dura BOS
-limite_eje_x = (1000* length(estimulos_tetrodos(1).canal(id_BOS).song) /...
-    estimulos_tetrodos(1).canal(id_BOS).freq) * 1.75;
+% limite_eje_x = (1000* length(estimulos_tetrodos(1).canal(id_BOS).song) /...
+%     estimulos_tetrodos(1).canal(id_BOS).freq) * 1.75;
+limite_eje_x = tiempo_file * 1000;
 
 % Formula para armar grilla segun la cantidad de estimulos a analizar
 if mod(length(id_estimulos), 3) == 0
