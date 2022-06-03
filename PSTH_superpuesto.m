336c9851f18c0cc935186e9bd7636f2c45dfec3b
Jav <javi.lassa@gmail.com>
2022-06-03 12:22:58 -0300

    graf superpuestos
    pequeñas mejoras

--- PSTH_superpuesto.m
+++ PSTH_superpuesto.m
@@ -1,9 +1,14 @@
-psth1 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/PSTHsw_1tet_BOS_P2-T2_BANDA-25-35Hz_-71uV.txt');
-psth2 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/PSTHsw_1tet_BOS_P3-T2_BANDA-25-35Hz_-72uV.txt');
-psth3 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/PSTHsw_1tet_BOS_P4-T1_BANDA-25-35Hz_-68uV.txt');
-psth4 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/PSTHsw_1tet_BOS_P1-T2_BANDA-25-35Hz_-67uV.txt');
-psth5 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/PSTHsw_1tet_BOS_P1-T3_BANDA-25-35Hz_-71uV.txt');
-psth6 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/PSTHsw_1tet_BOS_P1-T1_BANDA-25-35Hz_-61uV.txt');
+close all
+clear all
+
+directorio = '/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/';
+
+psth1 = readtable(strcat(directorio, 'PSTHsw_1tet_BOS_P2-T2_BANDA-25-35Hz_-71uV.txt'));
+psth2 = readtable(strcat(directorio, 'PSTHsw_1tet_BOS_P3-T2_BANDA-25-35Hz_-72uV.txt'));
+psth3 = readtable(strcat(directorio, 'PSTHsw_1tet_BOS_P4-T1_BANDA-25-35Hz_-68uV.txt'));
+psth4 = readtable(strcat(directorio, 'PSTHsw_1tet_BOS_P1-T2_BANDA-25-35Hz_-67uV.txt'));
+psth5 = readtable(strcat(directorio, 'PSTHsw_1tet_BOS_P1-T3_BANDA-25-35Hz_-71uV.txt'));
+psth6 = readtable(strcat(directorio, 'PSTHsw_1tet_BOS_P1-T1_BANDA-25-35Hz_-61uV.txt'));
 
 psth1 = psth1{:,1};
 psth2 = psth2{:,1};
@@ -12,48 +17,60 @@ psth4 = psth4{:,1};
 psth5 = psth5{:,1};
 psth6 = psth6{:,1};
 
-alpha = 0.25;
+alpha = 0.5;
+lw = 2;
 
+
+% Ploteo PSTH superpuestos
 figure()
-% p1 = plot(lfp1, 'LineWidth',5);
+p1 = plot(psth1, 'LineWidth', lw);
 hold on
-p2 = plot(psth2,'LineWidth',5);
+p2 = plot(psth2, 'LineWidth', lw);
 hold on
-% p3 = plot(lfp3,'LineWidth',5);
-% p4 = plot(psth4,':', 'LineWidth',5);
-p5 = plot(psth5, 'LineWidth', 5);
-p6 = plot(psth6);
+p3 = plot(psth3, 'LineWidth', lw);
+% p4 = plot(psth4, 'LineWidth', lw);
+% p5 = plot(psth5, 'LineWidth', lw);
+% p6 = plot(psth6, 'LineWidth', lw);
 
-% p1.Color(4) = alpha;
-% p2.Color(4) = alpha;
-% p3.Color(4) = alpha;
+p1.Color(4) = alpha;
+p2.Color(4) = alpha;
+p3.Color(4) = alpha;
 % p4.Color(4) = alpha;
-p5.Color(4) = alpha;
+% p5.Color(4) = alpha;
 
 % legend('P2-T2', 'P3-T2', 'P4-T1', 'P1-T2', 'P1-T3');
-legend('P3-T2', 'P1-T3', 'P1-T1');
+legend('P2-T2', 'P3-T2', 'P4-T1');
 title('PSTH promediando tetrodo para el BOS')
+set(gca,'FontSize',25)
 
 
-
+% Ploteo PSTH superpuestos normalizados
 figure()
-% p11 = plot(psth1/max(psth1),'LineWidth',5);
+p11 = plot(psth1/max(psth1), 'LineWidth', lw);
 hold on
-p22 = plot(psth2/max(psth2),'LineWidth',5);
+p22 = plot(psth2/max(psth2), 'LineWidth', lw);
 hold on 
-% p33 = plot(psth3/max(psth3),'LineWidth',5);
-% p44 = plot(psth4/max(psth4),':','LineWidth', 5);
-p55 = plot(psth5/max(psth5),'LineWidth', 5);
-p66 = plot(psth6/max(psth6));
+p33 = plot(psth3/max(psth3), 'LineWidth', lw);
+% p44 = plot(psth4/max(psth4), 'LineWidth', lw);
+% p55 = plot(psth5/max(psth5), 'LineWidth', lw);
+% p66 = plot(psth6/max(psth6), 'LineWidth', lw);
 
-% p11.Color(4) = alpha;
-% p22.Color(4) = alpha;
-% p33.Color(4) = alpha;
-p44.Color(4) = alpha;
-p55.Color(4) = alpha;
+p11.Color(4) = alpha;
+p22.Color(4) = alpha;
+p33.Color(4) = alpha;
+% p44.Color(4) = alpha;
+% p55.Color(4) = alpha;
 
 % legend('P2-T2', 'P3-T2', 'P4-T1', 'P1-T2', 'P1-T3');
-legend('P3-T2', 'P1-T3', 'P1-T1');
+legend('P2-T2', 'P3-T2', 'P4-T1');
 title('PSTH normalizado promediando tetrodo para el BOS')
+set(gca,'FontSize',25)
 
+% Guardo
+titulo = '_PSTH_promedioTetrodo_BOS';
 
+print_pdf(1, directorio, strcat(titulo, '_ZOOM.pdf'))
+print_pdf(2, directorio, strcat(titulo,'_normalizado_ZOOM', '.pdf'))
+
+% print_pdf(1, directorio, strcat(titulo, '.pdf'))
+% print_pdf(2, directorio, strcat(titulo,'_normalizado', '.pdf'))    
--- lfp_superpuesto.m
+++ lfp_superpuesto.m
@@ -1,9 +1,14 @@
-lfp1 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/LFP_1tet_BOS_P2-T2_BANDA-25-35Hz_-71uV.txt');
-lfp2 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/LFP_1tet_BOS_P3-T2_BANDA-25-35Hz_-72uV.txt');
-lfp3 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/LFP_1tet_BOS_P4-T1_BANDA-25-35Hz_-68uV.txt');
-lfp4 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/LFP_1tet_BOS_P1-T2_BANDA-25-35Hz_-67uV.txt');
-lfp5 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/LFP_1tet_BOS_P1-T3_BANDA-25-35Hz_-71uV.txt');
-lfp6 = readtable('/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/LFP_1tet_BOS_P1-T1_BANDA-25-35Hz_-61uV.txt');
+close all
+clear all
+
+directorio = '/Volumes/AUDIOS TEAM/Javi- finch durmierdo (NNx)/zf-JL037-VB/Datos/2021-12-21/zf-JL037-VB_p4_id1_211221_162306/';
+
+lfp1 = readtable(strcat(directorio, 'LFP_1tet_BOS_P2-T2_BANDA-25-35Hz_-71uV.txt'));
+lfp2 = readtable(strcat(directorio, 'LFP_1tet_BOS_P3-T2_BANDA-25-35Hz_-72uV.txt'));
+lfp3 = readtable(strcat(directorio, 'LFP_1tet_BOS_P4-T1_BANDA-25-35Hz_-68uV.txt'));
+lfp4 = readtable(strcat(directorio, 'LFP_1tet_BOS_P1-T2_BANDA-25-35Hz_-67uV.txt'));
+lfp5 = readtable(strcat(directorio, 'LFP_1tet_BOS_P1-T3_BANDA-25-35Hz_-71uV.txt'));
+lfp6 = readtable(strcat(directorio, 'LFP_1tet_BOS_P1-T1_BANDA-25-35Hz_-61uV.txt'));
 
 lfp1 = lfp1{:,1};
 lfp2 = lfp2{:,1};
@@ -19,69 +24,83 @@ env4 = abs(hilbert(lfp4));
 env5 = abs(hilbert(lfp5));
 env6 = abs(hilbert(lfp6));
 
-alpha = 0.25;
+alpha = 0.50;
+lw = 2;
 
+
+% LFP
 figure()
-% p1 = plot(lfp1, 'LineWidth',5);
+p1 = plot(lfp1, 'LineWidth', lw);
 hold on
-p2 = plot(lfp2,'LineWidth',5);
+p2 = plot(lfp2, 'LineWidth', lw);
 hold on
-% p3 = plot(lfp3,'LineWidth',5);
-p4 = plot(lfp4,':', 'LineWidth',5);
-p5 = plot(lfp5,':', 'LineWidth', 5);
-p6 = plot(lfp6);
+p3 = plot(lfp3, 'LineWidth', lw);
+% p4 = plot(lfp4, 'LineWidth', lw);
+% p5 = plot(lfp5, 'LineWidth', lw);
+% p6 = plot(lfp6, 'LineWidth', lw);
 
-% p1.Color(4) = alpha;
+p1.Color(4) = alpha;
 p2.Color(4) = alpha;
-% p3.Color(4) = alpha;
-p4.Color(4) = alpha;
-p5.Color(4) = alpha;
-
+p3.Color(4) = alpha;
+% p4.Color(4) = alpha;
+% p5.Color(4) = alpha;
 
 % legend('P2-T2', 'P3-T2', 'P4-T1', 'P1-T2', 'P1-T3');
-legend('P3-T2','P1-T2', 'P1-T3');
+legend('P2-T2', 'P3-T2', 'P4-T1');
 title('LFP banda 25-35Hz promediando tetrodo para el BOS')
+set(gca,'FontSize',25)
 
 
+% LFP env
 figure()
-% p11 = plot(env1,'LineWidth',5);
+p11 = plot(env1, 'LineWidth', lw);
 hold on
-p22 = plot(env2,'LineWidth',5);
+p22 = plot(env2, 'LineWidth', lw);
 hold on 
-% p33 = plot(env3,'LineWidth',5);
-p44 = plot(env4,':','LineWidth', 5);
-p55 = plot(env5,':','LineWidth', 5);
-p66 = plot(env6);
+p33 = plot(env3, 'LineWidth', lw);
+% p44 = plot(env4, 'LineWidth', lw);
+% p55 = plot(env5, 'LineWidth', lw);
+% p66 = plot(env6, 'LineWidth', lw);
 
-% p11.Color(4) = alpha;
+p11.Color(4) = alpha;
 p22.Color(4) = alpha;
-% p33.Color(4) = alpha;
-p44.Color(4) = alpha;
-p55.Color(4) = alpha;
+p33.Color(4) = alpha;
+% p44.Color(4) = alpha;
+% p55.Color(4) = alpha;
 
-% legend('P2-T2', 'P3-T2', 'P4-T1', 'P1-T2', 'P1-T3');
-legend('P3-T2','P1-T2', 'P1-T3');
+% legend('P3-T2','P1-T2','P1-T3','P1-T1');
+legend('P2-T2', 'P3-T2', 'P4-T1');
 title('LFP banda 25-35Hz promediando tetrodo para el BOS')
+set(gca,'FontSize',25)
 
 
+% LFP env normalizacion
 figure()
-% p11 = plot(env1/max(env1),'LineWidth',5);
+p11 = plot(env1/max(env1), 'LineWidth', lw);
 hold on
-p22 = plot(env2/max(env2),'LineWidth',5);
+p22 = plot(env2/max(env2), 'LineWidth', lw);
 hold on 
-% p33 = plot(env3/max(env3),'LineWidth',5);
-p44 = plot(env4/max(env4),':','LineWidth', 5);
-p55 = plot(env5/max(env5),':','LineWidth', 5);
-p66 = plot(env6/max(env6));
+p33 = plot(env3/max(env3), 'LineWidth', lw);
+% p44 = plot(env4/max(env4), 'LineWidth', lw);
+% p55 = plot(env5/max(env5), 'LineWidth', lw);
+% p66 = plot(env6/max(env6), 'LineWidth', lw);
 
-% p11.Color(4) = alpha;
+p11.Color(4) = alpha;
 p22.Color(4) = alpha;
-% p33.Color(4) = alpha;
-p44.Color(4) = alpha;
-p55.Color(4) = alpha;
+p33.Color(4) = alpha;
+% p44.Color(4) = alpha;
+% p55.Color(4) = alpha;
 
 % legend('P2-T2', 'P3-T2', 'P4-T1', 'P1-T2', 'P1-T3');
-legend('P3-T2','P1-T2', 'P1-T3');
+legend('P2-T2', 'P3-T2', 'P4-T1');
 title('LFP banda 25-35Hz promediando tetrodo para el BOS')
+set(gca,'FontSize',25)
 
+
+% Guardo
+titulo = '_LFP_promedioTetrodo_BOS_ZOOM';
+
+print_pdf(1, directorio, strcat(titulo, '.pdf'))
+print_pdf(2, directorio, strcat(titulo,'_env', '.pdf'))
+print_pdf(3, directorio, strcat(titulo,'_env-norm', '.pdf'))
 
