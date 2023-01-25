adf1e09bcaee4653c76ca67e02e79446e5fd5345
Jav <javi.lassa@gmail.com>
2023-01-25 17:37:15 -0300

    plot_some_sabana ERROR BAR
    Grafica error bar,  NO CHECKE QUE FUERA CORRECTO

--- plot_some_sabana.m
+++ plot_some_sabana.m
@@ -6,15 +6,34 @@ function plot_some_sabana(score_total, mat_avg, ejeX_fila, ejeY_col)
 
 % Ploteo integral
 figure()
+Z_all = zeros(length(score_total(1).grilla_scores(:,4)), ...
+    length(score_total(1)) );
 
 for i = (1:1:length(score_total))  
 X  = score_total(i).grilla_scores(:,1);
 Y  = score_total(i).grilla_scores(:,2);
 Z1 = score_total(i).grilla_scores(:,3);
+Z_all(:,i) = Z1;
+
 plot3(X,Y,Z1,'o')
 legends{i} = score_total(i).id;
 hold on
 end
+
+% Calculo error
+Z_std = zeros(size(Z_all, 1), 1);
+Z_mean = zeros(size(Z_all, 1), 1);
+for fila = (1:1:size(Z_all, 1))
+    Z_std(fila,1) = std(Z_all(fila, :));
+    Z_mean(fila,1) = mean(Z_all(fila, :));
+end 
+errl = Z_mean - Z_std;
+errh = Z_mean + Z_std;
+
+    
+plot3(X,Y,Z_mean, 'k*', 'MarkerSize',20 );
+plot3([X(:),X(:)]', [Y(:),Y(:)]', [errl(:),errh(:)]', '-r','LineWidth',5) 
+
 
 legends{length(legends) +1} = 'avg';
 
