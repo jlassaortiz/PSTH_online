359188741aa0bca833c689ac9bf5bcab9ac9d320
Jav <javi.lassa@gmail.com>
2023-02-23 15:43:07 -0300

    Create score_total_toTable.m
    transforma score_total en una tabla cuadrada linda para levantar con R o con lo que sea para hacer estadística

---
+++ score_total_toTable.m
@@ -0,0 +1,39 @@
+function tabla_all_datos = score_total_toTable(score_total, directorio_params)
+% Toma el objeto score_total (sale de score_all_and_plot)
+%   
+%   IN:
+%   score_total (tabla)
+%
+%   OUT:
+%   tabla_all_datos (tabla "cuadrada")
+
+protocolo = (1:length(score_total));
+all_lambdas = [0.5, 1.0, 1.5, 2.0, 2.5];
+
+id = [];
+lambda = [];
+integral = [];
+
+for p = protocolo
+    count = 1;
+    for l = all_lambdas
+        id = vertcat(id, convertCharsToStrings(score_total(p).id));
+        lambda = vertcat(lambda, l);
+        
+        index_int = score_total(p).grilla_scores(:,1) == count;
+        int = unique(score_total(p).grilla_scores(index_int, 3));
+        integral = vertcat(integral, int);
+        count = count + 1;
+    end   
+end 
+
+tabla_all_datos = table(id, lambda, integral);
+tabla_all_datos.integral = round(tabla_all_datos.integral, 3);
+
+time_stamp = convertCharsToStrings(datestr(now, "yyyy-mm-dd_HH_MM_SS"));
+
+writetable(tabla_all_datos, directorio_params + ...
+     time_stamp + "_tabla_all_datos.csv")
+
+end
+
